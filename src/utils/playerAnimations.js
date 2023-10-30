0ed32c45cea58ff86b39eacce7a20ab982067938
elkurud <samukama18@gmail.com>
2023-10-30 17:04:17 -0300

    chore: minor changes to structure

--- src/utils/playerAnimations.js
+++ src/utils/playerAnimations.js
@@ -1,52 +1,52 @@
 const animations = {
-    idleRight:{
-      frameRate:8,
-      frameBuffer:8,
-      loop:true,
-      imageSrc: './src/assets/idle.png',
-    },
-    idleLeft:{
-      frameRate:8,
-      frameBuffer:8,
-      loop:true,
-      imageSrc: './src/assets/idleLeft.png',
-    },
-    runRight:{
-      frameRate:8,
-      frameBuffer:7,
-      loop:true,
-      imageSrc: './src/assets/run.png',
-    },
-    runLeft:{
-      frameRate:8,
-      frameBuffer:7,
-      loop:true,
-      imageSrc: './src/assets/runLeft.png',
-    },
-    jump:{
-      frameRate:2,
-      frameBuffer:2,
-      loop:true,
-      imageSrc: './src/assets/jump.png',
-    },
-    fall:{
-      frameRate:2,
-      frameBuffer:2,
-      loop:true,
-      imageSrc: './src/assets/fall.png',
-    },
-    jumpLeft:{
-      frameRate:2,
-      frameBuffer:2,
-      loop:true,
-      imageSrc: './src/assets/jumpLeft.png',
-    },
-    fallLeft:{
-      frameRate:2,
-      frameBuffer:2,
-      loop:true,
-      imageSrc: './src/assets/fallLeft.png',
-    },
-  }
+  idleRight: {
+    frameRate: 8,
+    frameBuffer: 8,
+    loop: true,
+    imageSrc: "./src/assets/idle.png",
+  },
+  idleLeft: {
+    frameRate: 8,
+    frameBuffer: 8,
+    loop: true,
+    imageSrc: "./src/assets/idleLeft.png",
+  },
+  runRight: {
+    frameRate: 8,
+    frameBuffer: 7,
+    loop: true,
+    imageSrc: "./src/assets/run.png",
+  },
+  runLeft: {
+    frameRate: 8,
+    frameBuffer: 7,
+    loop: true,
+    imageSrc: "./src/assets/runLeft.png",
+  },
+  jump: {
+    frameRate: 2,
+    frameBuffer: 2,
+    loop: true,
+    imageSrc: "./src/assets/jump.png",
+  },
+  fall: {
+    frameRate: 2,
+    frameBuffer: 2,
+    loop: true,
+    imageSrc: "./src/assets/fall.png",
+  },
+  jumpLeft: {
+    frameRate: 2,
+    frameBuffer: 2,
+    loop: true,
+    imageSrc: "./src/assets/jumpLeft.png",
+  },
+  fallLeft: {
+    frameRate: 2,
+    frameBuffer: 2,
+    loop: true,
+    imageSrc: "./src/assets/fallLeft.png",
+  },
+};
 
-  export default animations
+export default animations;
